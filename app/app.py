63c35fe91ef141cd101b50929b807e3ed9fcb69b
kaio hursan <kaioh@pop-os.localdomain>
2019-06-17 15:30:18 -0300

    correção de bug

--- app/app.py
+++ app/app.py
@@ -10,6 +10,10 @@ def home():
 @app.route('/login')
 def login():
     return render_template('login.html')
+@app.route('/display')
+
+def display():
+    return render_template('display.html')
 
 if __name__=='__main__':
     app.run(debug=True)
